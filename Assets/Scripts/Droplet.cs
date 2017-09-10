b1da406e8c5e843faa584a7ed2d9c9e3a602f15c
Lego Chiang <qas612820704@gmail.com>
2017-09-10 12:19:17 +0800

    Oh, Droplet and Spwaner done

---
+++ Assets/Scripts/Droplet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Droplet : MonoBehaviour {
+
+	public ParticleSystem SprayParticle;
+
+	void OnCollisionEnter2D (Collision2D col) {
+		if (col.gameObject.tag == "Ground") {
+
+			ParticleSystem ps = Instantiate (SprayParticle, transform.position, transform.rotation) as ParticleSystem;
+			ps.Play ();
+
+
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	public float timeDelay;
+	public float timeSpwan;
+	public GameObject droplet;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("Spwan", timeDelay, timeSpwan);
+	}
+
+	void Spwan() {
+		Instantiate (droplet, transform.position, transform.rotation);
+	}
+}
